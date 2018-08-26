3fbb8f1863f484cf3090fa81ab8f26bbce061c45
Vitaly Yastremsky <vyastrem@gmail.com>
2018-08-26 18:20:09 +0300

    generate output

--- calculateValues.js
+++ calculateValues.js
@@ -1,36 +1,69 @@
 const { sortValues, toKWT } = require('./utils');
 const { getSupportedTarif } = require('./supportedTarifs');
-const DAY = 24;
-const HOURS_IN_ARRAY = new Array(DAY).fill(true);
 
-exports.getCalculatedValues = function(arr, initRates) {
-  return arr.map(item => ({ ...item, ...calcOptimalSheduleValues(item, initRates)}))
+
+exports.getCalculatedValues = function(arr, initRates, shedule) {
+  return arr.map(item => ({ ...item, ...calcOptimalSheduleValues(item, initRates, shedule)}))
 }
 
 function getSortableWorkShedule(device, rates) {
   return getSupportedTarif(device, rates).sort(sortValues).filter(item => item !== null);
 }
 
+
 function calculatedTimePeriod(sortableItem, duration, id) {
-    return HOURS_IN_ARRAY.map((item, index) => {
+    return HOURS_IN_ARRAY.reduce((curr, next, index) => {
         if(sortableItem.from + duration > 24) {
             if(index >= 0 && index < duration) {
-                return id;
+                next[index] = id;
             }
         } else if(index >= sortableItem.from && index < sortableItem.from + duration) {
-            return id;
+            curr[index] = id;
         }
-
-        return ""
-    })
+        curr[index] = "";
+        return {...curr}
+    }, {})
+}
+// console.log(generatedShedule)
+function fillShedule(value, shedule) {
+    for(hour in shedule) {
+        shedule[hour] = [value]
+        // shedule[hour].push(value)
+    }
+    return shedule;
 }
 
-function calcOptimalSheduleValues(device, rates) {
+function fillSheduleCondition(value, shedule, duration) {
+    for(hour in shedule) {
+        shedule[hour] = []
+        if(hour >= 0 && hour < duration) {
+            shedule[hour] = [value]
+            // shedule[hour].push(value)
+        }
+    }
+    return shedule;
+}
+
+function fillSheduleDevice(value, shedule, start, duration) {
+    for(hour in shedule) {
+        shedule[hour] = []
+        if(hour >= start && hour < start + duration) {
+            // shedule[hour] = [...shedule[hour], value]
+            shedule[hour] = [value]
+            // shedule[hour].push(value)
+        }
+    }
+    return shedule;
+}
+// console.log(generatedShedule)
+function calcOptimalSheduleValues(device, rates, ds) {
     const shedule = getSortableWorkShedule(device, rates)
+    const objs = Object.assign({}, ds)
+    // const objs = ds
     let period = 0;
     let result = 0;
-    let timePeriod = HOURS_IN_ARRAY;
-
+    let timePeriod = {};
+    // console.log(objs)
     for(let i = 0; i< shedule.length; i+=1) {
         period = shedule[i].to - shedule[i].from;
 
@@ -40,13 +73,18 @@ function calcOptimalSheduleValues(device, rates) {
         
         if(device.duration < period) {
             result = (toKWT(device.power) * device.duration) * shedule[i].value 
-            timePeriod = calculatedTimePeriod(shedule[i], device.duration, device.id)
+            if(shedule[i].from + device.duration > 24) {
+                timePeriod[device.id] = fillSheduleCondition(device.id, objs, device.duration)
+            }else {
+                timePeriod[device.id] =fillSheduleDevice(device.id, objs,shedule[i].from, device.duration)
+            }
             break;
         } else {
             result += (toKWT(device.power) * period) * shedule[i].value;
-            timePeriod = timePeriod.map(() => device.id)
+            timePeriod[device.id] = fillShedule(device.id, objs);
+            
         }
     }
-
+    // return objs
     return { result, timePeriod };
 }
--- index.js
+++ index.js
@@ -2,6 +2,37 @@ const input = require('./input.json');
 const { getCalculatedValues } = require('./calculateValues');
 const { devices, rates } = input;
 
-const data = getCalculatedValues(devices, rates);
+const DAY = 24;
+const HOURS_IN_ARRAY = new Array(DAY).fill(true);
 
-console.log(data)
+function generateShedule() {
+    return HOURS_IN_ARRAY.reduce((curr, next, i) => ({ ...curr, [i]: [] }), {})
+}
+
+const generatedShedule = generateShedule();
+const data = getCalculatedValues(devices, rates, generatedShedule);
+
+function generateConsumedEnergy(data) {
+    return data.reduce((curr, next) => {
+        
+        return Object.assign(curr, { 
+            value: next.result + curr.value, 
+            devices: { ...curr.devices, [next.id]: next.result 
+            }
+        });
+    }, { value: 0, devices: {} })
+}
+
+data.forEach(item => {
+    for(let key in item.timePeriod[item.id]) {
+        generatedShedule[key] = [...generatedShedule[key], ...item.timePeriod[item.id][key] ]
+    }
+})
+
+function generateResult(data) {
+    return {
+        shedule: generatedShedule,
+        consumedEnergy: generateConsumedEnergy(data)  
+    }
+}
+console.log(generateResult(data))
